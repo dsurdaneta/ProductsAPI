ba033f4ee40329bb76ca75995ab1dd54c2cafe6d
Urdaneta, Daniel <daniel.urdaneta@contractors.roche.com>
2019-05-10 13:23:27 +0200

    App_Start Folder small fixes

--- DsuDev.ProductsServerApi/App_Start/WebApiConfig.cs
+++ DsuDev.ProductsServerApi/App_Start/WebApiConfig.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
+﻿using System.Net.Http.Headers;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
-using Newtonsoft.Json.Serialization;
 
 namespace DsuDev.ProductsServerApi
 {
@@ -13,12 +8,12 @@ namespace DsuDev.ProductsServerApi
     {
         public static void Register(HttpConfiguration config)
         {
-			//json formatter
-			config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            //json formatter
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
-			// Web API configuration and services
-			// Configure Web API to use only bearer token authentication.
-			config.SuppressDefaultHostAuthentication();
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
             // Web API routes
@@ -29,6 +24,6 @@ namespace DsuDev.ProductsServerApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );	
-		}
+        }
     }
 }
