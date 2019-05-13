52d9f38d4495ebe506c2e3f4465adb6267133937
Urdaneta Daniel (ERNI) <urda@erni-espana.es>
2019-05-13 10:27:36 +0200

    undo deleted property in Domain.Entities.Products

--- ProductsApi/DsuDev.ProductsServerApi.Domain/Entities/Product.cs
+++ ProductsApi/DsuDev.ProductsServerApi.Domain/Entities/Product.cs
@@ -7,6 +7,7 @@ namespace DsuDev.ProductsServerApi.Domain.Entities
         public int Id { get; set; }
         [Required]
         public string ProductName { get; set; }
+        public string Category { get; set; }
         [DataType(DataType.Currency)]
         public decimal Price { get; set; }
         public int StockQuantity { get; set; }
