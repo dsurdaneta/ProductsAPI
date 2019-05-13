ee7a2e4bbb9d487124e2a2a10cf67650ce1c7426
Urdaneta Daniel (ERNI) <urda@erni-espana.es>
2019-05-13 10:40:31 +0200

    now Services is a .net core project instead of .net standard

---
+++ ProductsApi/DsuDev.ProductsServerApi.Services/Context/IProductServiceContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.Entity;
+using DsuDev.ProductsServerApi.Domain.Entities;
+
+namespace DsuDev.ProductsServerApi.Services.Context
+{
+    public interface IProductServiceContext : IDisposable
+    {
+        DbSet<Product> Products { get; }
+        DbSet<Category> Categories { get; }
+        int SaveChanges();
+        void MarkAsModified(Product item);
+    }
+}
