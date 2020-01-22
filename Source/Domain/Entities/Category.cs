066e78a793a33f8434debd781d43dd30042dc4f3
Urdaneta Daniel (ERNI) <daniel.udaneta@erni-espana.es>
2020-01-22 12:01:17 +0100

    added domain entities

---
+++ Source/Domain/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DsuDev.ProductsServer.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ Source/Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DsuDev.ProductsServer.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        public string Category { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+    }
+}
