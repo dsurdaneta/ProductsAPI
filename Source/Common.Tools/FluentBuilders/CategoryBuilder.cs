8ad54d4082643a82b937902bd47d6fce92bb6329
Urdaneta Daniel (ERNI) <daniel.udaneta@erni-espana.es>
2020-01-22 12:30:59 +0100

    added fluentbuilders

---
+++ Source/Common.Tools/FluentBuilders/CategoryBuilder.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using DsuDev.ProductsServer.Domain.Entities;
+
+namespace DsuDev.ProductsServer.Common.Tools.FluentBuilders
+{
+    public class CategoryBuilder
+    {
+        private int _id;
+        private string _name;
+        private List<Product> _products;
+
+        public CategoryBuilder CreateProduct()
+        {
+            this._id = 0;
+            this._name = string.Empty;
+            this._products = new List<Product>();
+            return this;
+        }
+
+        public CategoryBuilder WithId(int identifier)
+        {
+            this._id = identifier;
+            return this;
+        }
+
+        public CategoryBuilder WithName(string categoryName)
+        {
+            this._name = categoryName;
+            return this;
+        }
+
+        public CategoryBuilder WithProduct(Product product)
+        {
+            this._products.Add(product);
+            return this;
+        }
+        
+        public CategoryBuilder WithProductCollection(ICollection<Product> products)
+        {
+            this._products.AddRange(products);
+            return this;
+        }
+
+        public Category Build()
+        {
+            return new Category
+            {
+                Id = this._id,
+                CategoryName = this._name,
+                Products = this._products
+            };
+        }
+    }
+}
---
+++ Source/Common.Tools/FluentBuilders/ProductBuilder.cs
@@ -0,0 +1,65 @@
+﻿using DsuDev.ProductsServer.Domain.Entities;
+
+namespace DsuDev.ProductsServer.Common.Tools.FluentBuilders
+{
+    public class ProductBuilder
+    {
+        private int _id;
+        private string _name;
+        private string _category;
+        private decimal _price;
+        private int _quantity;
+
+        public ProductBuilder CreateProduct()
+        {
+            this._id = 0;
+            this._name = string.Empty;
+            this._category = string.Empty;
+            this._price = 0;
+            this._quantity = 0;
+            return this;
+        }
+
+        public ProductBuilder WithId(int identifier)
+        {
+            this._id = identifier;
+            return this;
+        }
+
+        public ProductBuilder WithName(string productName)
+        {
+            this._name = productName;
+            return this;
+        }
+
+        public ProductBuilder WithCategory(string productCategory)
+        {
+            this._category = productCategory;
+            return this;
+        }
+
+        public ProductBuilder WithPrice(decimal productPrice)
+        {
+            this._price = productPrice;
+            return this;
+        }
+
+        public ProductBuilder WithStockQuantity(int stockQuantity)
+        {
+            this._quantity = stockQuantity;
+            return this;
+        }
+
+        public Product Build()
+        {
+            return new Product
+            {
+                Id = this._id,
+                ProductName = this._name,
+                Category = this._category,
+                Price = this._price,
+                StockQuantity = _quantity
+            };
+        }
+    }
+}
